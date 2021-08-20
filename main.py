90d5af52170b5c912dbd7767a942f8f1d66afea7
Themi <Tcm4760@gmail.com>
2021-08-20 03:05:52 -0500

    -specified version numbers for socketio and engineio because newer versions aren't compatible with streamlabs's socket api -started basic foundation of main.py

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
 import socketio
-import requests
 
+with open("token.txt", "r") as f:
+    token = f.read()
+
+sio = socketio.Client(reconnection_delay_max=0)
+sio.connect(f"https://sockets.streamlabs.com?token={token}", transports=["websocket"])
+
+
+@sio.event()
+def connect():
+    print("Connected to streamlabs!")
+
+
+@sio.event()
+def connect_error():
+    print("Connection failed :\\")
+
+
+@sio.event()
+def disconnect():
+    print("Disconnected")
+
+
+@sio.on("event")
+def follow(data):
+    print(data)
