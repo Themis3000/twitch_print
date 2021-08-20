87b52198ab43f92b775251e5c51f8836dbaa3785
Themi <Tcm4760@gmail.com>
2021-08-20 03:30:45 -0500

    -Completed basic layout of main.py -Changed the dummy print method of printer.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import socketio
+from PIL import Image
+from pymitter import EventEmitter
 
 with open("token.txt", "r") as f:
     token = f.read()
 
 sio = socketio.Client(reconnection_delay_max=0)
 sio.connect(f"https://sockets.streamlabs.com?token={token}", transports=["websocket"])
+
+ee = EventEmitter()
 
 
 @sio.event()
@@ -23,5 +27,29 @@ def disconnect():
 
 
 @sio.on("event")
-def follow(data):
-    print(data)
+def event(data):
+    if "for" not in data or data["for"] == "twitch_account":
+        return
+
+    for message in data["message"]:
+        ee.emit(f"streamlabs.{data['type']}", message)
+
+
+@ee.on("streamlabs.follow")
+def follow_event(data):
+    pass
+
+
+@ee.on("streamlabs.subscription")
+def subscription_event(data):
+    pass
+
+
+@ee.on("streamlabs.bits")
+def bits_event(data):
+    pass
+
+
+@ee.on("streamlabs.raid")
+def raid_event(data):
+    pass
--- printer.py
+++ printer.py
@@ -1,3 +1,2 @@
 def print_img(image):
-    with open("out.jpg", "wb") as f:
-        f.write(image)
+    image.show()
